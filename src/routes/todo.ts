621ce876233633d80c6fb346c74e00b5b3eae2a8
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-13 13:21:42 +0200

    add delete functionality

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTodo, getTodo, updateToDo } from "../controller/todos";
+import { createTodo, deleteToDo, getTodo, updateToDo } from "../controller/todos";
 
 
 const router = Router();
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/', createTodo); // gets endpoint from app
 router.get('/', getTodo);
 router.patch('/:id', updateToDo);
-router.delete('/:id');
+router.delete('/:id', deleteToDo);
 
 export default router;
 // our waiter
